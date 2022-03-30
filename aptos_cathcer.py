044839c6d1e1a1e74d4b97b88a9be01b144eb3f6
Asynco <101818400+Asyncod@users.noreply.github.com>
2022-03-30 15:28:25 +0300

    Add comments

--- aptos_cathcer.py
+++ aptos_cathcer.py
@@ -4,12 +4,11 @@ from time import sleep
 import requests
 
 
-# id_group = "-1001447252368"
-id_group = "1095119526"
-# token = "5273864774:AAH-0ABpnglxbn8VyJqoM9rhBOatU3_kwD4"
-token = "1720011320:AAGI5SUo0AbwynunYicPJnpSff2o4Kjj81U"
-# chat_id_list = ["956692649430093904", "950849051358822431", "946124073400938497"]
-chat_id_list = ["958348625451307051", "958390758979821628"]
+
+id_group = "" # your ID or group ID (with -)
+token = "" # Bot's token from @BotFather
+chat_id_list = ["958348625451307051", "958390758979821628"] # Aptos's channel ID
+ds_token = "" # Discord from your acc
 
 
 
@@ -26,7 +25,8 @@ def test_inp():
 # SESSION FOR REQUESTS
 def session_create():
     session = requests.Session()
-    session.headers['Authorization'] = "OTM1ODI3NzE4NTY0NjE0MTU1.YkMBhw.dIDROR-adrSV_zrj57mRy4T-5ro"
+    session.headers['Authorization'] = ds_token
+    # Use another user agent or leave this one
     session.headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 YaBrowser/21.11.1.932 Yowser/2.5 Safari/537.36"
 
     return session
@@ -44,7 +44,10 @@ def check_work():
 
 def sender_info(msg):
     try:
-        msg = f"{msg}\n\n@asynco @jjouwie @Timq\_nnm @Portal286 @crinzhlove @kurilshik282 @jokilom @NeznaykoNn @iarsikk"
+        # If you receive updates in a chat with several people,
+        # then just add their ID to the message.
+        # Don't forget to escape "_"
+        msg = f"{msg}\n\n@asynco @Timq\_nnm"
         url = f"chat_id={id_group}&text={msg}&parse_mode=markdown"
         requests.get(f"https://api.telegram.org/bot{token}/sendMessage", url)
     except:
@@ -59,8 +62,8 @@ def check_news():
 
     while True:
         for link in chat_id_list:
-            mess = loads(session.get(f'https://discord.com/api/v9/channels/{link}/messages?limit=1').text)
-
+            mess = loads(session.get(f'https://discord.com/api/v9/channels/{link}/messages?limit=1').text) # get bebra
+            
             if mess[0]["id"] not in last_id:
                 last_id.append(mess[0]["id"])
                 sender_info(mess[0]["content"])
@@ -70,7 +73,7 @@ def check_news():
 
             sleep(3)
 
-        sleep(3669)
+        sleep(3669) # Time for sleep (this 1 hour, use another time or leave this)
         check_work()
 
 
